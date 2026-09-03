f37db7f3448914507b6c5eb372b9d8b4e8623a49
barmxds6ch <1773127066@qq.com>
2026-09-03 22:18:17 +0800

    chore: switch project references to barmxds6ch/PiliPlus

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,8 +1,8 @@
 abstract final class Constants {
   static const appName = 'PiliNara';
-  static const sourceCodeUrl = 'https://github.com/Starfallan/PiliNara';
-  static const upstreamCodeUrl = 'https://github.com/bggRGjQaUbCoE/PiliPlus';
-    
+  static const sourceCodeUrl = 'https://github.com/barmxds6ch/PiliPlus';
+  static const upstreamCodeUrl = 'https://github.com/Starfallan/PiliNara';
+
 
   // 27eb53fc9058f8c3  移动端 Android
   // 4409e2ce8ffd12b8  HD版
